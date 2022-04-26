cf1df6893eaadb0de6a0a6611cf91404cd450d2a
guosongqing <1084737312@qq.com>
2022-04-26 22:37:27 +0800

    第一次提交

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="hello">
+    {{ msg }}
+    <div class="btnaddreduce">
+      <button @click="btnx('add')">+</button>
+      <span>{{ coust }}</span>
+      <button @click="btnx('reduce')">-</button>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue, Emit } from "vue-property-decorator";
+
+@Component
+export default class HelloWorld extends Vue {
+  @Prop() private msg!: string;
+
+  @Emit("addToCount")
+  addToCount() {
+    this.coust++;
+  }
+
+  @Emit("resetCount")
+  resetCount() {
+    this.coust--;
+  }
+
+  private coust = 0;
+
+  private btnx(type:string){
+    if (type == "add") {
+      this.coust++;
+    } else {
+      this.coust--;
+    }
+  };
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.btnaddreduce {
+  width: 100px;
+  height: 26px;
+  border: 1px solid #cccccc;
+  display: flex;
+  justify-content: space-between;
+  text-align: center;
+  line-height: 26px;
+}
+button {
+  width: 26px;
+  height: 26px;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="home">
+    <HelloWorld :msg="title" @addToCount="add" @resetCount="reset" />
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import HelloWorld from "@/components/HelloWorld.vue";
+
+@Component({
+  name: "Home",
+  components: {
+    HelloWorld,
+  },
+})
+export default class extends Vue {
+  private title = "Welcome to Your Vue.js App";
+
+  add = () => {
+    alert();
+  };
+  reset = () => {
+    alert();
+  };
+
+  created() {
+    console.log("created");
+  }
+  mounted() {
+    console.log("mounted");
+  }
+  destroyed() {
+    console.log("destroyed");
+  }
+}
+</script>
+
